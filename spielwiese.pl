e0adfe250e4d143b978cc4fce5102f2bcd8fce96
carslen <carslen@emea.corpdir.net>
2017-05-29 10:34:13 +0200

    latest shutter function from Beta-User

---
+++ spielwiese.pl
@@ -0,0 +1,10 @@
+my $shutter = 123;
+if (!$shutter){
+    print "Kein Rollladen definiert"
+    }
+    elsif ($shutter eq "leer") {
+        print "Rollladen leer"
+    }
+    else {
+        print "$shutter"
+    };
