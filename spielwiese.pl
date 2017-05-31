ed0caed4a78d7cc7a64c5470bb6d409281b9d0e0
Carsten Lenz <cle@horaios.local>
2017-05-31 23:01:34 +0200

    20170531-2

--- spielwiese.pl
+++ spielwiese.pl
@@ -1,10 +1,29 @@
 my $shutter = leer123;
 if (!$shutter){
-    print "Kein Rollladen definiert"
+    print "Kein Rollladen definiert";
     }
     elsif ($shutter eq "leer") {
-        print "Rollladen leer"
+        print "Rollladen leer";
     }
     else {
-        print "$shutter"
+        print "$shutter";
     };
+
+my $reading = "set_33";
+my $out = index($reading,"set_");
+print $out;
+
+my @tfk=devspec2array("subType=threeStateSensor");
+my @windowOpen = undef;
+
+foreach(@tfk) {
+    $state = ReadingsVal($_,"state","nA");
+    if ($stage eq "open"){
+        push(@windowOpen,$_);
+    }
+}
+
+print "Es sind $#windowOpen Fenster offen!";
+
+{devspec2array("NAME=sec.tfk.og*:FILTER=state=open")}
+{@devices = devspec2array("subType=threeStateSensor");; foreach(@devices){print $_,"\n";;};;}
