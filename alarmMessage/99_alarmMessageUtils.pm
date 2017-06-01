a8a7c5207901231ba6911474a9d9ef4e4c03f56d
carslen <carslen@emea.corpdir.net>
2017-06-01 15:55:47 +0200

    spielwiese

--- alarmMessage/99_alarmMessageUtils.pm
+++ alarmMessage/99_alarmMessageUtils.pm
@@ -18,25 +18,39 @@ alarmMessageUtils_Initialize($$)
 
 # Enter you functions below _this_ line.
 
-sub alarmMessage_windowOpen($$) {
+sub openWindowAlarm() {
     # Diese Funktion sendet eine Benachrichtigung über offene Fenster per Telegramm, wenn die letzte Person das Haus verlässt, aber noch ein Fenster geöffnet ist.
     # notify reagiert auf Presence und tfk Events:
-    # defmod notifyName notify sec..*(closed|open|tilted)|(Rolladen_.*|Jalousie_.*).(motor:.stop.*|set_.*|motor..down.*) { functionName($NAME,$EVENT) }
+    # defmod notifyName notify sec..*(closed|open|tilted) { openWindowAlarm($NAME,$EVENT) }
 	my ($dev, $rawEvent) = @_;
 
-    # Variablen / Dinge merken
-    my @tfk = ("sec.tfk.eg.bad.links","sec.tfk.eg.bad.rechts","sec.tfk.eg.sz.li.01","sec.tfk.og.terrasse");
-    print @tfk, "\n";
-    print "blasfd $#tfk";
-
-    my @family = ("Robin","Carsten","Zelmira");
-    print @family, "\n";
-    print $#family, "\n";
-
-    foreach $name (@tfk){
-        print $name, "\n";
+    #Variablen definieren
+    my @tfk=devspec2array("subType=threeStateSensor");  # Welche Türfensterkontakte gibt es überhaupt
+    my @windowOpen = undef;
+    
+    foreach(@tfk) {
+        $state = ReadingsVal($_,"state","nA");
+        if ($stage eq "open") {
+            push(@windowOpen,$_);
+        }
     }
 
+print "Es sind $#windowOpen Fenster offen!";
+
+}
+
+
+sub homieReadingsAge() {
+    #Variablen definieren
+    my @homies = ("Carsten","Zelmira","Robin","Annika"); # Ggf. mittels devspec2array() füllen -> Sinnvolle Filter überlegen.
+    my %homieReadingsAge = undef; # Hash array definieren, welches später Bewohner und Readingsage enthält.
+    foreach(@homies){
+        if (ReadingsVal($_,"state",nA) eq "absent") {
+            $homie = $_;
+            $age   = ReadingsAge($state);
+            $homieReadingsAge{$homie} = "$age";
+        }
+    }
 }
 
 
--- spielwiese.pl
+++ spielwiese.pl
@@ -27,3 +27,49 @@ print "Es sind $#windowOpen Fenster offen!";
 
 {devspec2array("NAME=sec.tfk.og*:FILTER=state=open")}
 {@devices = devspec2array("subType=threeStateSensor");; foreach(@devices){print $_,"\n";;};;}
+
+my @homies = ("Carsten","Zelmira","Robin","Annika");
+my %readingsage = undef;
+foreach(@homies){
+    if (ReadingsVal($_,"state",nA) eq "absent") {
+        $homie = $_;
+        $age   = ReadingsAge($state);
+        $readingsage{$homie} = "$age";
+    }
+}
+print %ages;
+
+my @homies = ("Carsten","Zelmira","Robin","Annika");
+my $age  = "100";
+my %ages = undef;
+foreach(@homies){
+    $homie = $_;
+    $age   = --$age;
+    $ages{$homie} = "$age";
+    if ($age > "97"){
+        print "Der Mitbewohner $homie ist zuhause","\n";
+    }
+    else {
+        print "Der Mitbewohner $homie ist nicht zuhause", "\n";
+    }
+
+}
+print %ages;
+
+use List::Util qw(max);
+my %height = (
+    foo => 170,
+    bar => 181,
+    moo => 175,
+);
+
+my $highest = max values %height;
+ 
+print "$highest\n";
+
+foreach $entry (%height){
+    if ((value "$entry") = "$highest"){
+        $match = key $entry;
+        print "match ist $match";
+    }
+}
